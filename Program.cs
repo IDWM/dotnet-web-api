02074570c0f404779535906fa54e10806754b4c4
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-03-23 23:41:46 -0300

    feat: add middleware example

--- Program.cs
+++ Program.cs
@@ -6,4 +6,16 @@ var app = builder.Build();
 
 app.MapControllers();
 
+app.Use(
+    async (context, next) =>
+    {
+        var start = DateTime.UtcNow;
+
+        await next.Invoke();
+
+        var elapsed = DateTime.UtcNow - start;
+        Console.WriteLine($"Solicitud procesada en {elapsed.TotalMilliseconds}ms");
+    }
+);
+
 app.Run();
